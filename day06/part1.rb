b5d32883b1b7d6e1f0459bff1783624240c71a1a
Johannes Opper <xijo@pm.me>
2022-12-06 15:38:28 +0100

    day06, better

--- day06/part1.rb
+++ day06/part1.rb
@@ -1,20 +1,12 @@
 #!/usr/bin/env ruby
 # https://adventofcode.com/2022/day/6
 
-input = File.read('day06/input').strip.chars
+input = File.read('day06/example').strip.chars
 
 # part 1
-input.each_with_index do |char, index|
-  if input[([index-3, 0].max)..(index)].uniq.size == 4
-    puts index + 1
-    break
-  end
-end
+_c, i = input.each_with_index.find { |_c, index| input[([index-3, 0].max)..(index)].uniq.size == 4 }
+puts i + 1
 
 # part 2
-input.each_with_index do |char, index|
-  if input[([index-13, 0].max)..(index)].uniq.size == 14
-    puts index + 1
-    break
-  end
-end
+_c, i = input.each_with_index.find { |_c, index| input[([index-13, 0].max)..(index)].uniq.size == 14 }
+puts i + 1
